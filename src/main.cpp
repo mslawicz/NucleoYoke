8dac49b17ab9b5183508162153da59473e55902e
Marcin <slawicz@echostar.pl>
2019-08-02 12:17:01 +0200

    main.cpp edited; compiles OK

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,33 @@
   ******************************************************************************
 */
 
-
+#include "system.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo_144.h"
 			
-
 int main(void)
 {
+    // HAL and clock initialization
+    System::configMCU();
+    // create system singleton object and initialize hardware
+    System::getInstance().config();
 
-	for(;;);
+    // start reception of the first character
+//    System::getInstance().getConsole()->getInterface().startReception();
+
+    // send first prompt
+//    System::getInstance().getConsole()->sendPrompt();
+
+    // main loop
+    while(1)
+    {
+        System::getInstance().blinkSystemLED();
+
+//        System::getInstance().getConsole()->handler();
+//        System::getInstance().getHID()->test(); //XXX
+//        System::getInstance().getDisplay()->test();
+//        System::getInstance().getDisplay()->handler();
+    }
+
+    System::getInstance().terminate();
 }
