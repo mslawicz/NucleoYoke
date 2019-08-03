60334ba9abc5f5e889c8d96f8e7b4e8f845c8c30
Marcin <slawicz@echostar.pl>
2019-08-03 09:54:23 +0200

    Virtual COM files added

---
+++ src/virtualcom.cpp
@@ -0,0 +1,20 @@
+/*
+ * virtualcom.cpp
+ *
+ *  Created on: 03.08.2019
+ *      Author: Marcin
+ */
+
+#include "virtualcom.h"
+
+VirtualCom::VirtualCom()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+VirtualCom::~VirtualCom()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ src/virtualcom.h
@@ -0,0 +1,18 @@
+/*
+ * virtualcom.h
+ *
+ *  Created on: 03.08.2019
+ *      Author: Marcin
+ */
+
+#ifndef VIRTUALCOM_H_
+#define VIRTUALCOM_H_
+
+class VirtualCom
+{
+public:
+    VirtualCom();
+    ~VirtualCom();
+};
+
+#endif /* VIRTUALCOM_H_ */
