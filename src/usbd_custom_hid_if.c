33d3c41b866f364d48b4e71bf6b8bbc418ed0840
Marcin <slawicz@echostar.pl>
2019-08-06 21:54:32 +0200

    Nucleo Yoke in custom HID mode is recognized as game controller; tested OK

--- src/usbd_custom_hid_if.c
+++ src/usbd_custom_hid_if.c
@@ -90,27 +90,53 @@ uint8_t dataBuffer[CUSTOM_HID_EPOUT_SIZE];
 
 /** Usb HID report descriptor. */
 __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
-{
-  /* USER CODE BEGIN 0 */
-        0x06, 0x00, 0xff,               //Usage Page(Undefined )
-        0x09, 0x01,                    // USAGE (Undefined)
-        0xa1, 0x01,                    // COLLECTION (Application)
-        0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-        0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
-        0x75, 0x08,                    //   REPORT_SIZE (8)
-        0x95, 0x40,                    //   REPORT_COUNT (64)
-        0x09, 0x01,                    //   USAGE (Undefined)
-        0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-        0x95, 0x40,                    //   REPORT_COUNT (64)
-        0x09, 0x01,                    //   USAGE (Undefined)
-        0x91, 0x02,                    //   OUTPUT (Data,Var,Abs)
-        0x95, 0x01,                    //   REPORT_COUNT (1)
-        0x09, 0x01,                    //   USAGE (Undefined)
-        0xb1, 0x02,                    //   FEATURE (Data,Var,Abs)
-
-  /* USER CODE END 0 */
-  0xC0    /*     END_COLLECTION              */
-};
+        {
+                0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+                0x09, 0x04,                    // USAGE (Joystick)
+                0xa1, 0x01,                    // COLLECTION (Application)
+                0x05, 0x02,                    //   USAGE_PAGE (Simulation Controls)
+                0xa1, 0x00,                    //   COLLECTION (Physical)
+                0x09, 0xba,                    //     USAGE (Rudder)
+                0x09, 0xbb,                    //     USAGE (Throttle)
+                0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+                0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+                0x75, 0x08,                    //     REPORT_SIZE (8)
+                0x95, 0x02,                    //     REPORT_COUNT (2)
+                0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+                0xc0,                          //   END_COLLECTION
+                0x05, 0x01,                    //   USAGE_PAGE (Generic Desktop)
+                0x09, 0x01,                    //   USAGE (Pointer)
+                0xa1, 0x00,                    //   COLLECTION (Physical)
+                0x09, 0x30,                    //     USAGE (X)
+                0x09, 0x31,                    //     USAGE (Y)
+                0x75, 0x08,                    //     REPORT_SIZE (8)
+                0x95, 0x02,                    //     REPORT_COUNT (2)
+                0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+                0xc0,                          //   END_COLLECTION
+                0x09, 0x39,                    //   USAGE (Hat switch)
+                0x15, 0x01,                    //   LOGICAL_MINIMUM (1)
+                0x25, 0x08,                    //   LOGICAL_MAXIMUM (8)
+                0x35, 0x00,                    //   PHYSICAL_MINIMUM (0)
+                0x46, 0x3b, 0x01,              //   PHYSICAL_MAXIMUM (315)
+                0x65, 0x14,                    //   UNIT (Eng Rot:Angular Pos)
+                0x75, 0x04,                    //   REPORT_SIZE (4)
+                0x95, 0x01,                    //   REPORT_COUNT (1)
+                0x81, 0x42,                    //   INPUT (Data,Var,Abs, Null)
+                0x75, 0x04,                    //   REPORT_SIZE (4)
+                0x95, 0x01,                    //   REPORT_COUNT (1)
+                0x81, 0x41,                    //   INPUT (Cnst,Ary,Abs,Null)
+                0x05, 0x09,                    //   USAGE_PAGE (Button)
+                0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+                0x29, 0x20,                    //   USAGE_MAXIMUM (Button 32)
+                0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+                0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+                0x75, 0x01,                    //   REPORT_SIZE (1)
+                0x95, 0x20,                    //   REPORT_COUNT (32)
+                0x55, 0x00,                    //   UNIT_EXPONENT (0)
+                0x65, 0x00,                    //   UNIT (None)
+                0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+                0xc0                           // END_COLLECTION
+        };
 
 /* USER CODE BEGIN PRIVATE_VARIABLES */
 
@@ -192,9 +218,7 @@ static int8_t CUSTOM_HID_DeInit_FS(void)
 static int8_t CUSTOM_HID_OutEvent_FS(uint8_t* buffer)
 {
   /* USER CODE BEGIN 6 */
-    memcpy(dataBuffer, buffer, sizeof(dataBuffer));
-    dataBuffer[1] ^= 0x20;
-    USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, dataBuffer, sizeof(dataBuffer));
+    //handle host sent data here
     return (USBD_OK);
   /* USER CODE END 6 */
 }
