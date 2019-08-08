fec9f3c9dc51024e48864ee2e4a637349ab1b151
Marcin <slawicz@echostar.pl>
2019-08-08 19:54:39 +0200

    host output pipe tested OK

--- src/usbd_custom_hid_if.cpp
+++ src/usbd_custom_hid_if.cpp
@@ -77,6 +77,16 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
                 0x55, 0x00,                    //   UNIT_EXPONENT (0)
                 0x65, 0x00,                    //   UNIT (None)
                 0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+                // output data
+                0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+                0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
+                0x75, 0x08,                    //   REPORT_SIZE (8)
+                0x95, 0x40,                    //   REPORT_COUNT (64)
+                0x09, 0x01,                    //   USAGE (Undefined)
+                0x91, 0x02,                    //   OUTPUT (Data,Var,Abs)
+                0x95, 0x01,                    //   REPORT_COUNT (1)
+                0x09, 0x01,                    //   USAGE (Undefined)
+                0xb1, 0x02,                    //   FEATURE (Data,Var,Abs)
                 0xc0                           // END_COLLECTION
         };
 
@@ -129,7 +139,8 @@ static int8_t CUSTOM_HID_OutEvent_FS(uint8_t* buffer)
 {
   /* USER CODE BEGIN 6 */
     //handle host sent data here
-    System::getInstance().getConsole()->sendMessage(Severity::Info,LogChannel::LC_USB, "Sth received from host");
+    char* message = (char*)buffer;
+    System::getInstance().getConsole()->sendMessage(Severity::Info,LogChannel::LC_USB, "Message received from host: " + std::string(message));
     return (USBD_OK);
   /* USER CODE END 6 */
 }
