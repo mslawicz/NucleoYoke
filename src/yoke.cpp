9c31900b7d6f7d125667fe4dd22a1bdd8a00afda
Marcin <slawicz@echostar.pl>
2019-08-11 15:30:26 +0200

    yoke files added

---
+++ src/yoke.cpp
@@ -0,0 +1,20 @@
+/*
+ * yoke.cpp
+ *
+ *  Created on: 11.08.2019
+ *      Author: Marcin
+ */
+
+#include "yoke.h"
+
+Yoke::Yoke()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Yoke::~Yoke()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ src/yoke.h
@@ -0,0 +1,18 @@
+/*
+ * yoke.h
+ *
+ *  Created on: 11.08.2019
+ *      Author: Marcin
+ */
+
+#ifndef YOKE_H_
+#define YOKE_H_
+
+class Yoke
+{
+public:
+    Yoke();
+    ~Yoke();
+};
+
+#endif /* YOKE_H_ */
