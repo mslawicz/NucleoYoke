0a22a467c741bf5af1166b37ff66ef0afcb19719
Marcin <slawicz@echostar.pl>
2019-08-18 20:02:34 +0200

    report descriptor simpified for tests

--- inc/usbd_conf.h
+++ inc/usbd_conf.h
@@ -51,7 +51,7 @@
 #define USBD_SELF_POWERED                     1U
 #define USBD_DEBUG_LEVEL                      0U
 #define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     64U
-#define USBD_CUSTOM_HID_REPORT_DESC_SIZE      190U
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE      28U //190U
 
 /****************************************/
 /* #define for FS and HS identification */
