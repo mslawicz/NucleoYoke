3ff3b1670453ee5ffe8490badfc07d6d3868564c
Marcin <slawicz@echostar.pl>
2019-08-30 20:45:02 +0200

    I2C files added

---
+++ src/I2C.cpp
@@ -0,0 +1,9 @@
+/*
+ * I2C.cpp
+ *
+ *  Created on: 30.08.2019
+ *      Author: Marcin
+ */
+
+#include "I2C.h"
+
---
+++ src/I2C.h
@@ -0,0 +1,21 @@
+/*
+ * I2C.h
+ *
+ *  Created on: 30.08.2019
+ *      Author: Marcin
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+class I2cBus
+{
+
+};
+
+class I2cDevice
+{
+
+};
+
+#endif /* I2C_H_ */
