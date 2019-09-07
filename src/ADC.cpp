b600cb4597fe801f3080039655a47ee2a8690f24
Marcin <slawicz@echostar.pl>
2019-09-07 20:58:24 +0200

    ADC class and files created

---
+++ src/ADC.cpp
@@ -0,0 +1,15 @@
+/*
+ * ADC.cpp
+ *
+ *  Created on: 07.09.2019
+ *      Author: Marcin
+ */
+
+#include "ADC.h"
+
+ADC::ADC()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ src/ADC.h
@@ -0,0 +1,17 @@
+/*
+ * ADC.h
+ *
+ *  Created on: 07.09.2019
+ *      Author: Marcin
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+class ADC
+{
+public:
+    ADC();
+};
+
+#endif /* ADC_H_ */
