8124b40980719e6f76d804a0e362de35e05930a8
Marcin <slawicz@echostar.pl>
2019-09-08 15:22:15 +0200

    channel registration port map added

--- src/ADC.cpp
+++ src/ADC.cpp
@@ -7,11 +7,16 @@
 
 #include "ADC.h"
 #include "System.h"
+#include "GPIO.h"
+#include <unordered_map>
+#include <utility>
 
 uint32_t ADConverter::channelRank = 0;
 
 ADConverter::ADConverter()
 {
+    __HAL_RCC_ADC1_CLK_ENABLE();
+
     hADC.Instance = ADC1;
     hADC.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
     hADC.Init.Resolution = ADC_RESOLUTION_12B;
@@ -36,6 +41,11 @@ ADConverter::ADConverter()
     // register ADC channels
     registerChannel(ADC_CHANNEL_0);
     registerChannel(ADC_CHANNEL_8);
+
+    __HAL_RCC_DMA2_CLK_ENABLE();
+    /* DMA2_Stream0_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 }
 
 
@@ -44,6 +54,12 @@ ADConverter::ADConverter()
  */
 void ADConverter::registerChannel(uint32_t channel, uint32_t samplingTime)
 {
+    const std::unordered_map<uint32_t, std::pair<GPIO_TypeDef*, uint32_t>> ADCPorts =
+    {
+            {ADC_CHANNEL_0, std::pair<GPIO_TypeDef*, uint32_t>(GPIOA, GPIO_PIN_0)},
+            {ADC_CHANNEL_8, std::pair<GPIO_TypeDef*, uint32_t>(GPIOB, GPIO_PIN_0)}
+    };
+
     ADC_ChannelConfTypeDef config = {channel, ADConverter::channelRank++, samplingTime, 0};
     if (HAL_ADC_ConfigChannel(&hADC, &config) == HAL_OK)
     {
@@ -53,4 +69,5 @@ void ADConverter::registerChannel(uint32_t channel, uint32_t samplingTime)
     {
         System::getInstance().getConsole()->sendMessage(Severity::Error, LogChannel::LC_ADC, "ADC1 channel " + std::to_string(channel) + " registration failed");
     }
+    GPIO(ADCPorts.find(channel)->second.first, ADCPorts.find(channel)->second.second, GPIO_MODE_ANALOG, GPIO_NOPULL);
 }
