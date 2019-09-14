ba177cce2f8edf861dea4df42851c08c5ad801c3
Marcin <slawicz@echostar.pl>
2019-09-14 10:25:11 +0200

    Display class and files added

---
+++ src/Display.cpp
@@ -0,0 +1,15 @@
+/*
+ * Display.cpp
+ *
+ *  Created on: 14.09.2019
+ *      Author: Marcin
+ */
+
+#include "Display.h"
+
+Display::Display()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ src/Display.h
@@ -0,0 +1,17 @@
+/*
+ * Display.h
+ *
+ *  Created on: 14.09.2019
+ *      Author: Marcin
+ */
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+class Display
+{
+public:
+    Display();
+};
+
+#endif /* DISPLAY_H_ */
