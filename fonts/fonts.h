1fac3ea64cbf0bd4dd800288c65fa7f9015fe5bb
Marcin <slawicz@echostar.pl>
2019-09-15 10:58:25 +0200

    font files added

---
+++ fonts/fonts.h
@@ -0,0 +1,17 @@
+/*
+ * fonts.h
+ *
+ *  Created on: 27 lut 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_FONTS_FONTS_H_
+#define SRC_FONTS_FONTS_H_
+
+extern const uint8_t FontTahoma11[];
+extern const uint8_t FontTahoma11b[];
+extern const uint8_t FontTahoma14b[];
+extern const uint8_t FontTahoma15[];
+extern const uint8_t FontTahoma16b[];
+
+#endif /* SRC_FONTS_FONTS_H_ */
