177776d5e88c8ba4a0409bef0b6b3fbea45d864e
Marcin <slawicz@echostar.pl>
2019-09-21 09:51:37 +0200

    LSM9DS1 files added

---
+++ src/LSM9DS1.cpp
@@ -0,0 +1,15 @@
+/*
+ * LSM9DS1.cpp
+ *
+ *  Created on: 21.09.2019
+ *      Author: Marcin
+ */
+
+#include "LSM9DS1.h"
+
+LSM9DS1::LSM9DS1()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ src/LSM9DS1.h
@@ -0,0 +1,17 @@
+/*
+ * LSM9DS1.h
+ *
+ *  Created on: 21.09.2019
+ *      Author: Marcin
+ */
+
+#ifndef LSM9DS1_H_
+#define LSM9DS1_H_
+
+class LSM9DS1
+{
+public:
+    LSM9DS1();
+};
+
+#endif /* LSM9DS1_H_ */
