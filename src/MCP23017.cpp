e4c3a37ab262e67c69955853c74683b4e2699350
Marcin <slawicz@echostar.pl>
2019-10-03 22:08:29 +0200

    MCP23017 files added

---
+++ src/MCP23017.cpp
@@ -0,0 +1,15 @@
+/*
+ * MCP23017.cpp
+ *
+ *  Created on: 03.10.2019
+ *      Author: Marcin
+ */
+
+#include "MCP23017.h"
+
+MCP23017::MCP23017()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ src/MCP23017.h
@@ -0,0 +1,17 @@
+/*
+ * MCP23017.h
+ *
+ *  Created on: 03.10.2019
+ *      Author: Marcin
+ */
+
+#ifndef MCP23017_H_
+#define MCP23017_H_
+
+class MCP23017
+{
+public:
+    MCP23017();
+};
+
+#endif /* MCP23017_H_ */
