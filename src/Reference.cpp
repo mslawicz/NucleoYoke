de309874bc7b0761c2b69e75da152579e1d015ca
Marcin <slawicz@echostar.pl>
2019-12-12 18:59:07 +0100

    Reference class created

--- src/Reference.cpp
+++ src/Reference.cpp
@@ -7,9 +7,48 @@
 
 #include "Reference.h"
 
-Reference::Reference()
+Reference::Reference(uint32_t midValue, uint32_t referenceLimit, float referenceDeviation) :
+    midValue(midValue),
+    referenceLimit(referenceLimit),
+    referenceDeviation(referenceDeviation)
 {
-    // TODO Auto-generated constructor stub
-
+    noOfAveragedSamples = 0;
+    averageValue = 0.0f;
+    referenceValue = static_cast<float>(midValue);
 }
 
+// calculates reference according to input values
+void Reference::calculateReference(uint32_t inputValue)
+{
+    if ((inputValue > midValue - referenceLimit) &&
+        (inputValue < midValue + referenceLimit))
+    {
+        // input value in the allowed range of reference value
+        if (++noOfAveragedSamples == 1)
+        {
+            // the first valid sample
+            averageValue = static_cast<float>(inputValue);
+        }
+
+        if (fabs(inputValue - averageValue) < referenceDeviation)
+        {
+            // input value in allowed deviation reange
+            averageValue = (averageValue * (noOfAveragedSamples - 1) + inputValue) / noOfAveragedSamples;
+        }
+        else
+        {
+            // input value out of allowed deviation range
+            noOfAveragedSamples = 0;
+        }
+
+        if (noOfAveragedSamples > 15)   // XXX use different condition here
+        {
+            referenceValue = averageValue;
+        }
+    }
+    else
+    {
+        // input value out of allowed range
+        noOfAveragedSamples = 0;
+    }
+}
--- src/Reference.h
+++ src/Reference.h
@@ -11,7 +11,16 @@
 class Reference
 {
 public:
-    Reference();
+    Reference(uint32_t midValue, uint32_t referenceLimit, float referenceDeviation);
+    void calculateReference(uint32_t inputValue);
+    float getReference(void) const { return referenceValue; }
+private:
+    uint32_t midValue;              // middle (expected) value of the input range
+    uint32_t referenceLimit;        // allowed limit of measured reference value
+    float referenceDeviation;       // allowed deviation from measured reference value
+    uint32_t noOfAveragedSamples;   // number of averaged samples
+    float averageValue;             // samples average value being calculated
+    float referenceValue;           // measured and accepted reference value
 };
 
 #endif /* REFERENCE_H_ */
