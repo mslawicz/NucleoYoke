a81204d6c879d41d2a8048d5eaa18863b021e182
Marcin <slawicz@echostar.pl>
2019-12-12 18:54:22 +0100

    Reference files added

---
+++ src/Reference.cpp
@@ -0,0 +1,15 @@
+/*
+ * Reference.cpp
+ *
+ *  Created on: 12.12.2019
+ *      Author: Marcin
+ */
+
+#include "Reference.h"
+
+Reference::Reference()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ src/Reference.h
@@ -0,0 +1,17 @@
+/*
+ * Reference.h
+ *
+ *  Created on: 12.12.2019
+ *      Author: Marcin
+ */
+
+#ifndef REFERENCE_H_
+#define REFERENCE_H_
+
+class Reference
+{
+public:
+    Reference();
+};
+
+#endif /* REFERENCE_H_ */
